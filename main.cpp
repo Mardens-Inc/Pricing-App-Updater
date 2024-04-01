69eb29d77f272f1c6688605713b873e734742b34
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-01 13:35:06 -0400

    Add main.cpp for updating pricing-app executable
    This commit introduces a new file, main.cpp, primarily designed to update the pricing-app executable. The code ensures that the existing version of the application is properly terminated before copying a new version to the same location. It further validates the existence of the new executable before performing these operations.

---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <filesystem>
+#include <string>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: pricing-app-updater.exe <path_to_tmp_exe> <path_to_current_exe> \n");
+        return 1;
+    }
+    std::string tmp_exe_path = argv[1];
+    std::string current_exe_path = argv[2];
+
+    if (!std::filesystem::exists(tmp_exe_path)) {
+        fprintf(stderr, "Error: %s does not exist\n", tmp_exe_path.c_str());
+        return 1;
+    }
+
+    // kill the current exe
+    std::string command = "taskkill /F /IM pricing-app.exe";
+    system(command.c_str());
+    // delete current exe
+    if (std::filesystem::exists(current_exe_path))
+        std::filesystem::remove(current_exe_path);
+    // move current exe to current directory
+    std::filesystem::copy(tmp_exe_path, current_exe_path);
+    std::filesystem::remove(tmp_exe_path);
+    // start the new exe in the background
+    std::system(current_exe_path.c_str());
+    return 0;
+}
